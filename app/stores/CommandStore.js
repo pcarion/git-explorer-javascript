18a500061574b663792a4ae65acf02f1f856c531
Pierre Carion <pcarion@qce.qualcomm.com>
2015-01-16 10:49:07 -0800

    execute command line

--- app/stores/CommandStore.js
+++ app/stores/CommandStore.js
@@ -3,8 +3,26 @@
   var EventEmitter = require('events').EventEmitter;
   var AppConstants = require('../constants/AppConstants');
   var util = require('util');
+  var exec = require('child_process').exec;
 
   var CHANGE_EVENT = 'change';
+
+
+  function executeCommand(cmd) {
+    console.log("execute command:" + cmd);
+
+    var child = exec(cmd,
+      function(error, stdout, stderr) {
+        console.log("" + stdout);
+
+        if (stderr.size > 0) {
+          console.log('stderr: ' + stderr);
+        }
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+      });
+  }
 
   function CommandStore() {
     EventEmitter.call(this);
@@ -29,8 +47,18 @@
   };
 
   // Register callback to handle all updates
-  AppDispatcher.register(function(action) {
-    console.log("@@@ CommandStore: ", action)
+  AppDispatcher.register(function(payload) {
+    var action = payload.action;
+    console.log("@@@ CommandStore: ", action);
+
+    switch (action.type) {
+      case AppConstants.ActionTypes.NEW_COMMAND:
+        executeCommand(action.text.trim());
+        break;
+
+      default:
+        // no op
+    }
   });
 
   module.exports = new CommandStore();
